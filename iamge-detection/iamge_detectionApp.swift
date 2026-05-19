12eb92c631c843a4dc761e9584b68b39870279bf
adjiemuliadi <adjiem31@gmail.com>
2026-05-20 01:49:12 +0800

    Initial Commit

---
+++ iamge-detection/iamge_detectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  iamge_detectionApp.swift
+//  iamge-detection
+//
+//  Created by Abuidillah Adjie Muliadi on 20/05/26.
+//
+
+import SwiftUI
+
+@main
+struct iamge_detectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
